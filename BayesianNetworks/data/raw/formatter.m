93922aa97fa23b2bd395cef24ec1b2d6b98424b5
Angel Simeonov <angel.st.sim@gmail.com>
2020-03-03 13:46:26 +0000

    formatter for intelligent dungeon::room pairing + removed count nets because we have unknown values

---
+++ BayesianNetworks/data/raw/formatter.m
@@ -0,0 +1,27 @@
+[dNames,~,dId] = unique(lidungeondata.Name);
+[rNames,~,rId] = unique(roomdata.Name);
+%hardcoded unknown values to 100, remove afterwards!!! Also append the
+%headers again...
+dungeons = lidungeondata{:,[2,3]};
+rooms = roomdata{:,[2,3,4]};
+
+combined = [];
+
+for i = 1:length(rId)
+    combined = [combined; [rooms(i,:) dungeons(find(rId(i) == dId),:)]];
+end
+
+n = floor(length(combined)/3);
+
+r1 = [1:1:n];
+r2 = [n+1:1:n*2];
+r3 = [(n*2)+1:1:length(combined)];
+
+p1 = combined(r1,:);
+p2 = combined(r2,:);
+p3 = combined(r3,:);
+
+writematrix(p1, "DR_1.csv");
+writematrix(p2, "DR_2.csv");
+writematrix(p3, "DR_3.csv");
+% writematrix(combined,"DR_data.csv");
